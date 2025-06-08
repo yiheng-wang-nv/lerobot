99fb61e7f6a06b1688c77e1e1982eedf98bd7976
Yiheng Wang <vennw@nvidia.com>
2025-06-08 03:40:33 +0000

    update
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- train_soarm101.sh
+++ train_soarm101.sh
@@ -7,3 +7,12 @@ python lerobot/scripts/train.py \
   --job_name=so101_pick_and_place_home_fix_video \
   --policy.device=cuda \
   --wandb.enable=false
+
+pip install -e ".[pi0]"
+python lerobot/scripts/train.py \
+  --dataset.repo_id=${HF_USER}/so101_pick_and_place_home_fix_video \
+  --policy.type=pi0fast \
+  --output_dir=outputs/train/so101_pick_and_place_home_fix_video_pi0fast \
+  --job_name=so101_pick_and_place_home_fix_video_pi0fast \
+  --policy.device=cuda \
+  --wandb.enable=false
